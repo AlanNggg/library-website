14b69571eceaae6599a90fc5cad6752673f35fd0
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-10-18 16:13:36 +0800

    Add files via upload

---
+++ HUMASS/js/library.js
@@ -0,0 +1,28 @@
+input([
+      {
+        "Name": "Eng Book",
+        "Author": "Chris Wong",
+        "Description": "It is a book about English.",
+        "ISBN": "12345678"
+      },
+      {
+        "Name": "Chinese Book",
+        "Author": "Chris Wong",
+        "Description": "It is a book about Chinese.",
+        "ISBN": "23456789"
+      },
+      {
+        "Name": "Maths Book",
+        "Author": "Chris Chan",
+        "Description": "It is a book about Mathematics.",
+        "ISBN": "34567890"
+      },
+      {
+        "Name": "Biology Book",
+        "Author": "Cherry Fung",
+        "Description": "It is a book about Biology.",
+        "ISBN": "45678901"
+      }
+    
+]
+)
