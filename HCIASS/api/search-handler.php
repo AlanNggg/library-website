288e243d2f3b56009c49c04ecc78dc2d3ffa4908
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-20 00:49:36 +0800

    Update search-handler.php

--- HCIASS/api/search-handler.php
+++ HCIASS/api/search-handler.php
@@ -7,13 +7,78 @@
     $keyword = $data->keyword;
     
     $link = mysqli_open();
+    if ($table == "All" && is_null($keyword)) {
+        $query = array();
+        $query[] = "SELECT * FROM book";
+        $query[] = "SELECT * FROM magazine";
+        $query[] = "SELECT * FROM software";
 
-    $query = "SELECT * FROM $table WHERE name LIKE '%$keyword%'";
-    $result = $link->query($query);
+        $result = array();
+
+        foreach ($query as $sql) {
+            $result[] = $link->query($sql);
+        }
+        
+        
+        $search = array();
+    } else if ($table == "All") {
+        $query = array();
+        $query[] = "SELECT * FROM book WHERE name LIKE '%$keyword%'";
+        $query[] = "SELECT * FROM magazine WHERE name LIKE '%$keyword%'";
+        $query[] = "SELECT * FROM software WHERE name LIKE '%$keyword%'";
+
+        $result = array();
+
+        foreach ($query as $sql) {
+            $result[] = $link->query($sql);
+        }
+
+    } else if (is_null($keyword)) {
+
+        $query = "SELECT * FROM $table";
     
-    $search = array();
+        $result = $link->query($query);
+        
+        $search = array();
 
-    if (mysqli_num_rows($result) > 0) {
+    } else {
+        $query = "SELECT * FROM $table WHERE name LIKE '%$keyword%'";
+    
+        $result = $link->query($query);
+        
+        $search = array();
+
+    }
+
+    if (is_array($result)) {
+        foreach ($result as $ans) {
+            while ($row = mysqli_fetch_assoc($ans)) {
+                $search[] = $row;
+            }
+        }
+
+        mysqli_close($link);
+
+        if (sizeof($search) > 0) {
+            echo json_encode(
+                array(
+                        "stat"=>"SUCCESS",
+                        "search"=>$search
+            ));
+            return;
+        } else {
+            echo json_encode(
+                array(
+                        "stat"=>"FAILED",
+                        "num" => mysqli_num_rows($result),
+                        "table" => $table,
+                        "keyword" => $keyword,
+                        "query" => $query
+            ));
+            return;
+        }
+
+    } else if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $search[] = $row;
         }
@@ -23,17 +88,18 @@
         echo json_encode(
             array(
                     "stat"=>"SUCCESS",
-                    "search"=>$search
+                    "search"=>$search,
+                    "table" => $table
         ));
         return;
     } else {
         echo json_encode(
             array(
-                    "stat"=>"FAIL",
-                    "error" => mysqli_error($link),
+                    "stat"=>"FAILED",
                     "num" => mysqli_num_rows($result),
-                    "table" => $attribute,
-                    "keyword" => $keyword
+                    "table" => $table,
+                    "keyword" => $keyword,
+                    "query" => $query
         ));
         return;
     }
