d965b9a864a27c5210198502faf6880d0e5cc267
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-22 13:00:57 +0800

    Update result.php

--- HCIASS/result.php
+++ HCIASS/result.php
@@ -456,6 +456,7 @@
     }
 
 </style>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="http://kenwheeler.github.io/slick/slick/slick.css" rel="stylesheet"/>
 <link href="http://kenwheeler.github.io/slick/slick/slick-theme.css" rel="stylesheet"/>
 <script src="js/jquery-3.3.1.js"></script>
